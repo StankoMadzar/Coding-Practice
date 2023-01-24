eaf1199d911b44faad85bb20c621833f09db085c
Stanko Madzar <stanko.madzar@outlook.com>
2023-01-24 21:39:24 +0100

    Records

---
+++ War Preparations/War Preparations/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Sword originalSword = new Sword(SwordMaterial.iron, GemstoneType.none, 120, 20);
+Sword betterSword = originalSword with { gemstone = GemstoneType.emerald };
+Sword evenBetterSword = originalSword with { material = SwordMaterial.steel, gemstone = GemstoneType.diamond };
+
+Console.WriteLine(originalSword.ToString());
+Console.WriteLine(betterSword.ToString());
+Console.WriteLine(evenBetterSword.ToString());
+
+
+public record Sword (SwordMaterial material, GemstoneType gemstone, int length, int crossguardWidth);
+
+public enum GemstoneType { none, emerald, amber, sapphire, diamond, bitsone };
+public enum SwordMaterial { wood, bronze, iron, steel, binarium };
